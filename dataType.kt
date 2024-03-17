eebed9b9e4f5f49e5f740e611334dcecab141065
kumarans <mrkumarancse@gmail.com>
2024-03-17 13:09:42 +0530

    Initial

---
+++ dataType.kt
@@ -0,0 +1,23 @@
+/*
+Kotlin datatypes
+ String
+ Int
+ Double
+ Float
+ Boolean
+
+*/
+
+fun main() {
+    var Integer: Int = 10
+    var Text: String = "This is an example of TEXT"
+    var doubleInt: Double = 12346576.87653
+    var floatInt: Float = 231.3F
+    var bool: Boolean = true
+    var pet = "Bird" // Declaring Variable without defining it's Datatype
+    println("This is an Integer : $Integer")
+    println("This is an String : $Text")
+    println("This is an DoubleInteger : $doubleInt")
+    println("This is an FloatInteger : $floatInt")
+    println("This is an Boolean : $bool")
+}
